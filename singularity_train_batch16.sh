5f97d88f892930736cbdb0dc0d9f97797bf0b880
Michael Stanley <michael-stanley@hotmail.com>
2022-07-20 14:53:52 +1200

    new generate bash script

--- singularity_train_batch16.sh
+++ singularity_train_batch16.sh
@@ -5,15 +5,12 @@
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=stanlemich@myvuw.ac.nz
 #SBATCH --time=23:00:00
-#SBATCH -o /nfs/scratch2/stanlemich/sing_generate.out
-#SBATCH -e /nfs/scratch2/stanlemich/sing_generate.err
+#SBATCH -o /nfs/scratch2/stanlemich/sing_train_batch16.out
+#SBATCH -e /nfs/scratch2/stanlemich/sing_train_batch16.err
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=2
 #SBATCH --mem=80g
-
-
-module load fosscuda/2020b
 
 module load singularity
 
@@ -27,8 +24,7 @@ module load singularity
 nvidia-smi
 
 #for chch data
-singularity run --nv singularity/singularity.simg python /nfs/scratch/stanlemich/projects/generate.py --outdir="/nfs/scratch/stanlemich/data/generated_images/wt2_autocrop_out_130722_batch16" --trunc=1 --seeds=1000-6000 --network="/nfs/scratch2/stanlemich/data/trained_models/wt2_autocrop_out_070622_batch16/00002-wt2_autocrop_aug_070622-auto1-batch16-resumecustom/network-snapshot-005200.pt"  
-#--resume "/nfs/scratch2/stanlemich/data/trained_models/wt2_autocrop_out_070622_batch16/00001-wt2_autocrop_aug_070622-auto1-batch16-resumecustom/network-snapshot-005200.pt"
+singularity run --nv singularity/singularity.simg python /nfs/scratch/stanlemich/projects/train.py --outdir "/nfs/scratch/stanlemich/data/trained_models/wt2_mixcrop_out_220622_batch16" --data "/nfs/scratch2/stanlemich/data/augmented_trainsets/wt2_ALL_CROP_220622.zip"  --cfg=auto --workers=1 --gpus=1  --batch=16 --metrics=fid50k --resume "/nfs/scratch2/stanlemich/data/trained_models/wt2_mixcrop_out_220622_batch16/00000-wt2_ALL_CROP_220622-auto1-batch16/network-snapshot-005200.pt"
 
 #singularity run --nv singularity/singularity.simg python /nfs/scratch/stanlemich/projects/train.py --outdir "/nfs/scratch/stanlemich/data/trained_models/chch_seginf_out_050522_batch16" --data "/nfs/scratch/stanlemich/data/seginf_crop_chch/train.zip"  --cfg=auto --workers=1 --gpus=1  --batch=16 --metrics=fid50k --resume "/nfs/scratch2/stanlemich/data/trained_models/chch_seginf_out_050522/00006-train-auto1-batch16-resumecustom/network-snapshot-005000.pt"
 
